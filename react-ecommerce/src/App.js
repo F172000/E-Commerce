0f75e8fcc06eb193cac7832af30dff7720272811
Fazila <fazilabano17@gmail.com>
2023-08-10 17:27:27 +0500

    checkout page and routes are created

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -11,6 +11,7 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
 const router=createBrowserRouter([{
   path:'/',
   element:<LoginPage/>
@@ -26,6 +27,10 @@ const router=createBrowserRouter([{
 {
 path:'/cart',
 element:<CartPage/>
+},
+{
+  path:'/checkout',
+  element:<Checkout/>
 }])
 function App() {
   return (
