4f65ec0aa7265856997bfc86d0de43046404ac05
Fazila <fazilabano17@gmail.com>
2023-08-17 18:29:09 +0500

    brand logo is added

--- react-ecommerce/src/features/Navbar/Navbar.js
+++ react-ecommerce/src/features/Navbar/Navbar.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { useNavigate,Link } from 'react-router-dom'
+import { useNavigate,Link } from 'react-router-dom';
+import logo from '../images/logo.png';
 
 const user = {
   name: 'Tom Cook',
@@ -11,7 +12,7 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'E-commerce', href: '#', current: true },
+  { name: 'Aura', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
 ]
 const userNavigation = [
@@ -37,8 +38,8 @@ export default function Navbar({children}){
                     <div className="flex-shrink-0">
                       <Link to={'/home'}>
                       <img
-                        className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                        className="h-12 w-12"
+                        src={logo}
                         alt="Your Company"
                       />
                       </Link>
@@ -189,7 +190,7 @@ export default function Navbar({children}){
 
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">E-Commerce</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Aura</h1>
           </div>
         </header>
         <main>
